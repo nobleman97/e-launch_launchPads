c2df4ef2616aee09b27e28dcdc6b203faf2f54b5
David <davidstone097@gmail.com>
2024-11-15 15:29:13 +0100

    fix burn function and improve record-keeping

--- contracts/voip/voipBridge.sol
+++ contracts/voip/voipBridge.sol
@@ -5,17 +5,17 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IERC20 {
     function balanceOf(address account) external view returns (uint256);
-    function _burn(address account, uint256 amount) external returns (bool);
+    // function _burn(address account, uint256 amount) external returns (bool);
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
     function transfer(address recipient, uint256 amount) external returns (bool);
 }
 
-contract VoipBridge is Ownable{
+contract VoipBridge is Ownable {
     IERC20 public voipToken;
     address public relayer;
     bool public contractIsPaused;
 
-    mapping(address => uint256) public lockedBalances;
+    mapping(address => mapping(string => uint256)) public lockedBalances;
     mapping(string => bool) public solanaAddressIsUsed;
  
     event TokensBurned(address indexed user, uint256 amount, uint256 timestamp);
@@ -38,27 +38,26 @@ contract VoipBridge is Ownable{
         require(
             voipToken.transferFrom(msg.sender, address(this), _amount), "Token transfer failed"
         );
-
-        lockedBalances[msg.sender] += _amount;
-        solanaAddressIsUsed[_solanaAddress] = true;
+        lockedBalances[msg.sender][_solanaAddress] += _amount;
 
         emit TokensLocked(_amount, msg.sender, _solanaAddress, block.timestamp);
     }
 
-    function burnTokens(address _user) external onlyOwner {
-        require(lockedBalances[_user] > 0, "Amount must be greater than zero");
+    function burnTokens(address _user,  string memory _solanaAddress) external onlyOwner {
+        require(lockedBalances[_user][_solanaAddress] > 0, "Amount must be greater than zero");
 
-        lockedBalances[_user] = 0;
-        voipToken._burn(_user, lockedBalances[_user]);
+        lockedBalances[_user][_solanaAddress] = 0;
+        solanaAddressIsUsed[_solanaAddress] = true;
+        voipToken.transfer(address(0x000000000000000000000000000000000000dEaD), lockedBalances[_user][_solanaAddress]);
 
-        emit TokensBurned(_user, lockedBalances[_user], block.timestamp);
+        emit TokensBurned(_user, lockedBalances[_user][_solanaAddress], block.timestamp);
     }
 
-    function unlockTokens (address _user) external onlyOwner {
-        require(lockedBalances[_user] > 0, "Insufficient balance locked up in contract");
+    function unlockTokens (address _user,  string memory _solanaAddress) external onlyOwner {
+        require(lockedBalances[_user][_solanaAddress] > 0, "Insufficient balance locked up in contract");
 
-        lockedBalances[_user] = 0;
-        voipToken.transfer(_user, lockedBalances[_user]);
+        lockedBalances[_user][_solanaAddress] = 0;
+        voipToken.transfer(_user, lockedBalances[_user][_solanaAddress]);
     }
 
     function isValidSolanaAddress(string memory solanaAddress) public pure returns (bool) {
